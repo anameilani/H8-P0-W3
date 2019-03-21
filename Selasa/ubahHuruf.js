f9ec1274450737955c2951207d64fc43cfbeba3d
anameilani <meilani.ana@gmail.com>
2019-03-21 15:21:27 +0700

    revisi ubahHuruf

--- Selasa/ubahHuruf.js
+++ Selasa/ubahHuruf.js
@@ -1,14 +1,19 @@
 function ubahHuruf(kata) {
   // you can only write your code here!
-  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
       var tambahKata=''
   for(var i=0; i<kata.length; i++){
       for(var j=0; j<alphabet.length; j++){
-        if(kata[i]== alphabet[j]){
+           
+        if(kata[i]== alphabet[j] && kata[i] != alphabet[alphabet.length-1]){
           tambahKata= tambahKata+ alphabet[j+1]
+            
+        }else if(kata[i] == alphabet[j] && kata[i] == alphabet[alphabet.length-1]){
+          tambahKata += alphabet[0]
+         
         }
+        
       }
   }
 
@@ -17,7 +22,7 @@ function ubahHuruf(kata) {
 }
 
 // TEST CASES
-console.log(ubahHuruf('wow')); // xpx
+console.log(ubahHuruf('woz')); // xpx
 console.log(ubahHuruf('developer')); // efwfmpqfs
 console.log(ubahHuruf('javascript')); // kbwbtdsjqu
 console.log(ubahHuruf('keren')); // lfsfo
