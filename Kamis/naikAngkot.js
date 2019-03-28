c139ac53160306469277cc64c458247ab1355c41
anameilani <meilani.ana@gmail.com>
2019-03-28 09:53:22 +0700

    revisi naikAngkot

--- Kamis/naikAngkot.js
+++ Kamis/naikAngkot.js
@@ -1,46 +1,46 @@
 function naikAngkot(arrPenumpang) {
-  rute = ['A', 'B', 'C', 'D', 'E', 'F'];
-  //your code here
-  
-
-  if(arrPenumpang == undefined){
-      return arrPenumpang
-  }
-
-  var perjalanan=[]
-   
-    for(var i=0; i<arrPenumpang.length; i++){
-    var dataPenumpang={
-        'penumpang': arrPenumpang[i][0],
-        'naikDari': arrPenumpang[i][1],
-        'tujuan': arrPenumpang[i][2],
-        'biaya': tarif,  
-    }
-
-    var indexNaik= 0
-    var indexTujuan= 0
+    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    //your code here
     
-     for(var j=0; j<rute.length; j++){
-         if(arrPenumpang[i][1] === rute[j]){
-             indexNaik = j
-         }
-         if(arrPenumpang[i][2]== rute[j]){
-             indexTujuan = j
-         }
-     }
+  
+    if(arrPenumpang == undefined){
+        return arrPenumpang
+    }
+  
+    var perjalanan=[]
      
-     var tarif= (indexTujuan-indexNaik)*2000
-     perjalanan.push(dataPenumpang)
-     dataPenumpang.biaya= tarif    
+      for(var i=0; i<arrPenumpang.length; i++){
+      var dataPenumpang={
+          'penumpang': arrPenumpang[i][0],
+          'naikDari': arrPenumpang[i][1],
+          'tujuan': arrPenumpang[i][2],
+          'biaya': tarif,  
+      }
+  
+      var indexNaik= 0
+      var indexTujuan= 0
+      
+       for(var j=0; j<rute.length; j++){
+           if(arrPenumpang[i][1] === rute[j]){
+               indexNaik = j
+           }
+           if(arrPenumpang[i][2]== rute[j]){
+               indexTujuan = j
+           }
+       }
+       
+       var tarif= Math.abs(indexTujuan-indexNaik)*2000
+       perjalanan.push(dataPenumpang)
+       dataPenumpang.biaya= tarif    
+    }
+  
+    return perjalanan
+  
   }
-
-  return perjalanan
-
-}
-
-//TEST CASE
-console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
-// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
-//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
-
-console.log(naikAngkot([])); //[]
+  
+  //TEST CASE
+  console.log(naikAngkot([['Dimitri', 'F', 'A'], ['Icha', 'A', 'B']]));
+  // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+  //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+  
+  console.log(naikAngkot([])); //[]
